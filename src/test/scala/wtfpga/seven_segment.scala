dc14ee45409e4be01408c4b025a1e03b990e91d5
Marco Neumann <marco@crepererum.net>
2020-06-19 15:03:30 +0200

    test+fix mux

--- src/test/scala/wtfpga/seven_segment.scala
+++ src/test/scala/wtfpga/seven_segment.scala
@@ -7,6 +7,59 @@ import chisel3.tester._
 import chisel3.experimental.BundleLiterals._
 
 
+class SevenSegMuxTesters extends FlatSpec with ChiselScalatestTester {
+    behavior of "SevenSegMux"
+
+    it should "support 7 bits" in {
+        test(new SevenSegMux()) { mod =>
+            mod.io.disp0.poke(127.U)
+            mod.io.disp1.poke(127.U)
+            mod.io.segout.expect(127.U)
+
+            mod.io.disp0.poke(126.U)
+            mod.io.disp1.poke(126.U)
+            mod.io.segout.expect(126.U)
+        }
+    }
+
+    it should "switch outputs" in {
+        test(new SevenSegMux()) { mod =>
+            mod.io.disp0.poke(1.U)
+            mod.io.disp1.poke(2.U)
+
+            mod.io.disp_sel.expect(false.B)
+            mod.io.segout.expect(1.U)
+
+            mod.clock.step()
+            mod.io.disp_sel.expect(true.B)
+            mod.io.segout.expect(2.U)
+
+            mod.clock.step()
+            mod.io.disp_sel.expect(false.B)
+            mod.io.segout.expect(1.U)
+        }
+    }
+
+    it should "not buffer" in {
+        test(new SevenSegMux()) { mod =>
+            mod.io.disp0.poke(1.U)
+            mod.io.disp1.poke(1.U)
+            mod.io.segout.expect(1.U)
+
+            mod.clock.step()
+            mod.io.disp0.poke(2.U)
+            mod.io.disp1.poke(2.U)
+            mod.io.segout.expect(2.U)
+
+            mod.clock.step()
+            mod.io.disp0.poke(3.U)
+            mod.io.disp1.poke(3.U)
+            mod.io.segout.expect(3.U)
+        }
+    }
+}
+
+
 class NibbleToSevenSegTesters extends FlatSpec with ChiselScalatestTester {
     behavior of "NibbleToSevenSeg"
 
