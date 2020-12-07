f53130d68f2bb8bdb511c6c1b4600d067094f2b6
Marco Neumann <marco@crepererum.net>
2020-12-07 22:12:24 +0100

    fix IO naming

--- src/main/scala/wtfpga/main.scala
+++ src/main/scala/wtfpga/main.scala
@@ -2,17 +2,29 @@ package wtfpga
 
 import chisel3._
 import chisel3.util._
+import chisel3.util.experimental.forceName
 
 class WTFpga extends RawModule {
-  val clock   = IO(Input(Clock()))    suggestName("CLK")
-  val i_btn1  = IO(Input(Bool()))     suggestName("BTN1")
-  val i_btn2  = IO(Input(Bool()))     suggestName("BTN2")
-  val i_btn3  = IO(Input(Bool()))     suggestName("BTN3")
-  val i_btn_n = IO(Input(Bool()))     suggestName("BTN_N")
-  val i_sw    = IO(Input(UInt(8.W)))  suggestName("sw")
-  val o_led   = IO(Output(UInt(5.W))) suggestName("led")
-  val o_seg   = IO(Output(UInt(7.W))) suggestName("seg")
-  val o_ca    = IO(Output(Bool()))    suggestName("ca")
+  val clock   = IO(Input(Clock()))
+  val i_btn1  = IO(Input(Bool()))
+  val i_btn2  = IO(Input(Bool()))
+  val i_btn3  = IO(Input(Bool()))
+  val i_btn_n = IO(Input(Bool()))
+  val i_sw    = IO(Input(UInt(8.W)))
+  val o_led   = IO(Output(UInt(5.W)))
+  val o_seg   = IO(Output(UInt(7.W)))
+  val o_ca    = IO(Output(Bool()))
+
+  // force names of external signals to match PCF
+  forceName(clock,   "CLK")
+  forceName(i_btn1,  "BTN1")
+  forceName(i_btn2,  "BTN2")
+  forceName(i_btn3,  "BTN3")
+  forceName(i_btn_n, "BTN_N")
+  forceName(i_sw,    "sw")
+  forceName(o_led,   "led")
+  forceName(o_seg,   "seg")
+  forceName(o_ca,    "ca")
 
   // Mock reset signal
   val nothing = Wire(Bool())
